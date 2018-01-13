2c32d76e2009e289f46c693bfe30d8a7c72a266d
Alli Tzadok <allisontzadok@gmail.com>
2018-01-13 20:29:24 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,13 +17,10 @@ def fourth_challenge
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
+      epic_tragedy[:montague][:hero][:status] = "dead"
+      epic_tragedy[:capulet][:heroine][:status] = "dead"
    }
-   epic_tragedy[:montague][:hero][:status] = "dead"
-   epic_tragedy[:capulet][:heroine][:status] = "dead"
+
 }
-
-
-
-
 
 end
