3d8d0d69cbb0285726864e0b1a6b5a06fdbf0ac3
Alli Tzadok <allisontzadok@gmail.com>
2018-01-13 20:41:43 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,6 +11,7 @@ def fourth_challenge
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
+   binding.pry
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
